30fb1540ef065a807f3ed343c40cc69b2188e403
Azusa_Rn <319812598@qq.com>
2026-04-15 15:51:57 +0800

    Initial commit

---
+++ Core/App/Inc/main.h
@@ -0,0 +1,26 @@
+/************************************************************
+ * ��Ȩ��2025CIMC Copyright�� 
+ * �ļ���Headerfiles.h
+ * ����: Lingyu Meng
+ * ƽ̨: 2025 CIMC IHD V04
+ * �汾: Lingyu Meng     2023/2/16     V0.01    original
+************************************************************/
+
+#ifndef __MAIN_H
+#define __MAIN_H
+
+/************************* ͷ�ļ� *************************/
+
+#include "gd32f4xx.h"
+#include "gd32f4xx_libopt.h"
+#include "systick.h"
+#include <stdio.h>
+#include <stdint.h>
+#include "string.h"
+#include "Function.h"
+#include "../../Bsp/LED.h"
+
+#endif
+
+/****************************End*****************************/
+
